a819a98f2ae9b2c7f5d28fe2874b2f2e9ac823e8
mauriciopr17@gmail.com <mauriciopr17@gmail.com>
2021-08-12 13:22:32 +0000

    Commit ajuste fact_vendas.sql

--- models/staging/stg_motivo_venda.sql
+++ models/staging/stg_motivo_venda.sql
@@ -2,7 +2,12 @@ WITH MOTIVO_VENDA AS (
 						  --**-- MOTIVO DE VENDA 
 						 SELECT ROW_NUMBER() OVER ( ORDER BY S.SALESREASONID ) SK_MOTIVO_VENDA 
 						 	   ,S.SALESREASONID       AS ID_MOTIVO_VENDA
-						 	   ,S.NAME		          AS DESCRICAO_MOTIVO_VENDA
+						 	   ,CASE 
+								  WHEN ( S.NAME	IS NOT NULL ) THEN
+								    S.NAME
+								  ELSE
+								    'Others'
+								  END DESCRICAO_MOTIVO_VENDA
 						 	   ,S.REASONTYPE 		  AS TIPO_VENDA 
 						 	   ,S.MODIFIEDDATE        AS DATA_MODIFICACAO_MOTIVO_VENDA
                            FROM {{ source('adventure_works', 'salesreason' )}} S ) 
